d30c4dd5feb7900d2c9878d69a7e75745dbac12f
Rodrigobran <141784903+Rodrigobran@users.noreply.github.com>
2023-09-25 09:57:47 -0600

    Add files via upload

---
+++ SEMANA 8 LABORATORIO 1132223 RODRIGO BRAN SÁNCHEZ/L8RB1132223/L8RB1132223/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace L8RB1132223
+    class Program
+    {
+            static void Main()
+            {
+                Console.Write("Laboratorio Semana No.08");
+                Console.WriteLine("\nIngrese un número");
+
+                int N = Convert.ToInt32(Console.ReadLine());
+
+                int A = 0, B = 1, C = 0, i = 2;
+                string resultado = "";
+                // 
+
+                if (N > 0)
+                {
+                    resultado = A.ToString();
+                    if (N > 1)
+                    {
+                        resultado += B.ToString();
+
+                        while (i < N)
+                        {
+                            C = A + B;
+                    resultado += B.ToString();
+                    A = B;
+                    A = C;
+                    i++;
+                         }
+
+                Console.WriteLine("Resultado" + resultado);
+                    }
+            else
+            {
+            Console.WriteLine("Resultado" + resultado);
+            }
+             }
+        else
+        {
+            Console.WriteLine("Resultado" + resultado);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
