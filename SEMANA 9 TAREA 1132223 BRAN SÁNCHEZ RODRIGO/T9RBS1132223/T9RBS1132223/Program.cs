9ca5cafae9e0ff2ce92e2bce8b6e07de147c377b
Rodrigobran <141784903+Rodrigobran@users.noreply.github.com>
2023-10-06 20:45:17 -0600

    Add files via upload

---
+++ SEMANA 9 TAREA 1132223 BRAN SÁNCHEZ RODRIGO/T9RBS1132223/T9RBS1132223/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número entero mayor a 0: ");
+        int numero;
+        while (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0)
+        {
+            Console.Write("Por favor, ingrese un número entero mayor a 0: ");
+        }
+
+        // Serie 1: (1/1) + (1/2) + (1/3) + ... + (1/numero)
+        double serie1 = 0.0;
+        for (int i = 1; i <= numero; i++)
+        {
+            serie1 += 1.0 / i;
+        }
+
+        // Serie 2: (1/2^1) + (1/2^2) + (1/2^3) + ... + (1/2^numero)
+        double serie2 = 0.0;
+        int exponente = 1;
+        while (exponente <= numero)
+        {
+            serie2 += 1.0 / Math.Pow(2, exponente);
+            exponente++;
+        }
+
+        Console.WriteLine("Serie 1: " + serie1);
+        Console.WriteLine("Serie 2: " + serie2);
+
+        Console.ReadLine();
+    }
+}
+
