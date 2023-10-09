46515d138b6e4bc11d2a886b6d1fc84feb2ccaab
Rodrigobran <141784903+Rodrigobran@users.noreply.github.com>
2023-10-09 09:43:01 -0600

    Add files via upload

---
+++ SEMANA 10 LABORATORIO 1132223 BRAN SÁNCHEZ RODRIGO/L10RBS1132223/L10RBS1132223/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Automovil auto_1 = new Automovil();
+
+        Console.WriteLine("Modelo: " + auto_1.modelo);
+        Console.WriteLine("Precio: " + auto_1.precio);
+        Console.WriteLine("Marca: " + auto_1.marca);
+        Console.WriteLine("Disponible: " + auto_1.disponible);
+        Console.WriteLine("Tipo cambio dolar: " + auto_1.tipoCambioDolar);
+        Console.WriteLine("Descuento aplicado: " + auto_1.descuentoAplicado);
+
+
+        Console.ReadKey();
+    }
+}
+
+//creación de la clase Estudiante
+public class Automovil
+{
+    //Atributos de la clase
+    public int modelo { get; set; }
+    public double precio { get; set; }
+    public string marca { get; set; }
+    public bool disponible { get; set; }
+    public double tipoCambioDolar { get; set; }
+    public double descuentoAplicado { get; set; }
+
+    //Constructor
+    public Automovil()
+    {
+        this.modelo = 2019;
+        this.precio = 10000.00;
+        this.marca = "";
+        this.disponible = false;
+        this.tipoCambioDolar = 7.50;
+        this.descuentoAplicado = 0.00;
+
+    }
+
+}
